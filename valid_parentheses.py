904ee7106df36839aa6d05b9dee73c33489c9ba9
philipsinnott <philipsinnotts@gmail.com>
2022-11-06 13:28:24 +0100

    valid_parentheses solution w/ stack

--- valid_parentheses.py
+++ valid_parentheses.py
@@ -2,37 +2,25 @@
 # 1: Open brackets must be closed by the same type of brackets.
 # 2: Open brackets must be closed in the correct order.
 # 3: Every close bracket has a corresponding open bracket of the same type.
-# STACK?
 
 def isValid(s: str) -> bool:
+    stack = []
+
     valid =\
     {
         "(": ")",
         "{": "}",
         "[": "]"
     }
-    count = isValidHelper(s)
-    for i in count:
-        if (i % 2 != 0):
-            return False
 
-    for i in range(0, len(s)-1, 2):
-        if (valid.get(s[i]) != s[i+1]):
-            return False
-    return True
-
-def isValidHelper(s: str) -> tuple:
-    reg, cur, squ = 0, 0, 0
-    re, cu, sq = ["(", ")"], ["{", "}"], ["[", "]"]
     for i in s:
-        if i in re:
-            reg += 1
-        elif i in cu:
-            cur += 1
-        elif i in sq:
-            squ += 1
-    return reg, cur, squ
-
+        if i in valid.keys(): # (['(', '{', '['])
+            stack.append(valid[i])
+        elif len(stack) == 0 or stack[-1] != i:
+            return False
+        else:
+            stack.pop()
+    return len(stack) == 0
 
 ex = "{[]}"
 print(isValid(ex))
